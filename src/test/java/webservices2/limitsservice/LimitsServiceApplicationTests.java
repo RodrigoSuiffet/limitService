23fcdd4d5dc0e6e87cbd097d542348f4d90d62b7
Rodrigo Gustavo Suiffet <rodrigo.gustavo.suiffet@met.ey.com>
2021-12-28 15:48:04 +0100

    Creado el proyecto inicial

---
+++ src/test/java/webservices2/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package webservices2.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
